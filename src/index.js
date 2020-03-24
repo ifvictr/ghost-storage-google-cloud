738ea648c3ffe09ca8cae915814383d9e6cea224
Victor Truong <victor@victortruong.com>
2020-03-24 14:44:05 -0700

    Make uploading work

--- src/index.js
+++ src/index.js
@@ -1,38 +1,46 @@
 import { Storage } from '@google-cloud/storage'
 import Promise from 'bluebird'
 import BaseStorage from 'ghost-storage-base'
+import isUrl from 'is-url'
 import path from 'path'
+import { URL } from 'url'
+import * as utils from './utils'
+
+const GOOGLE_STORAGE_URL = 'https://storage.googleapis.com'
 
 class GoogleCloudStorage extends BaseStorage {
     constructor(config) {
         super()
 
-        const bucketName = process.env.GHOST_GOOGLE_CLOUD_BUCKET_NAME || config.bucketName
+        // Required config
+        const bucketName = process.env.GHOST_GOOGLE_CLOUD_BUCKET || config.bucket
+
+        // Optional config
+        const baseUrl = utils.removeTrailingSlashes(process.env.GHOST_GOOGLE_CLOUD_BASE_URL || config.baseUrl || '')
+        this.baseUrl = isUrl(baseUrl)
+            ? baseUrl
+            : `${GOOGLE_STORAGE_URL}/${bucketName}`
+        this.maxAge = process.env.GHOST_GOOGLE_CLOUD_MAX_AGE || config.maxAge || (60 * 60 * 24 * 365) // Default to one year
+        this.projectId = process.env.GHOST_GOOGLE_CLOUD_PROJECT_ID || config.projectId
 
         const client = new Storage()
-        this.bucket = client.bucket(bucketName)
+        this.bucket = client.bucket(bucketName, { userProject: this.projectId })
     }
 
     delete(filename, targetDir) {
-        // TODO: Figure out if you need to handle response
-        return this.getFile(path.join(targetDir, filename)).delete()
+        return this.bucket.file(path.join(targetDir, filename)).delete()
     }
 
     exists(filename, targetDir) {
-        return this.getFile(path.join(targetDir, filename)).exists()
+        return this.bucket.file(path.join(targetDir, filename)).exists()
             .then(res => res[0])
-            .catch(e => false)
     }
 
     read(options) {
-        // TODO: Ensure this is correct
         return new Promise((resolve, reject) => {
             const data = []
-            this.getFile(options.path).createReadStream()
-                .on('data', chunk => {
-                    console.log(chunk)
-                    data.push(chunk)
-                })
+            this.bucket.file(options.path).createReadStream()
+                .on('data', data.push)
                 .on('end', () => {
                     resolve(Buffer.concat(data))
                 })
@@ -46,24 +54,31 @@ class GoogleCloudStorage extends BaseStorage {
         return this.getUniqueFileName(file, dir)
             .then(filename => {
                 const options = {
-                    destination: path.join(dir, filename)
+                    destination: filename,
+                    metadata: {
+                        cacheControl: `public, max-age=${this.maxAge}`,
+                        contentType: file.type
+                    },
+                    public: true
                 }
-                return this.bucket.upload(path.join(dir, filename), options)
+                return this.bucket.upload(file.path, options)
             })
-            .then(res => {
-                // TODO: Return file URL
-            })
+            .then(res => this.getUrl(res[0].name))
             .catch(Promise.reject)
     }
 
     serve() {
+        // No need to serve because absolute URLs are returned
         return (req, res, next) => {
             next()
         }
     }
 
-    getFile(filename) {
-        return this.bucket.file(filename)
+    getUrl(filepath) {
+        const url = new URL(this.baseUrl)
+        url.pathname = `${utils.removeTrailingSlashes(url.pathname)}/${filepath}`
+
+        return url.toString()
     }
 }
 
---
+++ src/utils.js
@@ -0,0 +1,1 @@
+export const removeTrailingSlashes = str => str.replace(/\/+$/, '')
